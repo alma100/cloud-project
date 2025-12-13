6986f99b23437a83f055dfb475ae71dc692287c9
alma100 <89401657+alma100@users.noreply.github.com>
2025-12-14 00:27:20 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,11 @@
 set -e
 
 apt-get update -y
-snap install kubectl --classic
+KUBECTL_VERSION="v1.34.0"
+
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin/kubectl
 curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 apt-get install -y nginx
